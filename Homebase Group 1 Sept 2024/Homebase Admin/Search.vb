7c953fb7bef240e4f31dda6a71b8942fbf6d021c
Hajar Maisarah <maisarahashari@gmail.com>
2026-05-25 01:09:17 +0800

    Update Search.vb

--- Homebase Group 1 Sept 2024/Homebase Admin/Search.vb
+++ Homebase Group 1 Sept 2024/Homebase Admin/Search.vb
@@ -2,7 +2,7 @@
 Imports MySql.Data.MySqlClient
 
 Public Class Search
-    Dim connectionString As String = "Server=110.4.43.110;Database=homebasegponecs;Uid=maisarah;Pwd=maisarah;"
+    Dim connectionString As String = "server credentials"
     Private Sub btnUser_Click(sender As Object, e As EventArgs) Handles btnUser.Click
 
         user_list.Show()
